ce0fba26409149e1afc7f41c457c601d934c901a
yokra9 <53964890+yokra9@users.noreply.github.com>
2022-09-11 07:08:36 +0000

    テストケースの追加とリファクタリング

--- src/test/scala/MainTests.scala
+++ src/test/scala/MainTests.scala
@@ -3,33 +3,47 @@ import cats.effect.unsafe.IORuntime
 import java.util.concurrent.Executors
 import org.http4s.client._
 import org.http4s.ember.client._
+import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers._
+import org.scalatest.prop._
+import scala.collection.immutable._
 
-class MainTests extends AnyFunSuite {
+class MainTests extends AnyFunSuite with should.Matchers {
   implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
 
   var step = 0
   test(s"${step}: サーバが起動すること") {
     val server = Main.start()
-
     val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
     val req = httpClient.expect[String]("http://localhost:8080/index")
     val res = req.unsafeRunSync()
-    assert(res.contains("param: Map()"))
-
     server.stop()
+
+    res should include("param: Map()")
   }
 
   step += 1
-  test(s"${step}: 環境変数でポート指定ができること") {
+  test(s"${step}: システムプロパティでポート指定ができること") {
     sys.props("http.port") = "8082"
     val server = Main.start()
-
     val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
     val req = httpClient.expect[String]("http://localhost:8082/index")
     val res = req.unsafeRunSync()
-    assert(res.contains("param: Map()"))
-
     server.stop()
+
+    res should include("param: Map()")
+  }
+
+  step += 1
+  test(s"${step}: システムプロパティで異常値が設定されていても 8080 にフォールバックされること") {
+    sys.props("http.port") = "Int に変換できない値"
+    val server = Main.start()
+    val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
+    val req = httpClient.expect[String]("http://localhost:8080/index")
+    val res = req.unsafeRunSync()
+    server.stop()
+
+    res should include("param: Map()")
   }
 }
